de23d2e6eef0e966059c70f6cfd0d4ec2e600e7e
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2018-09-01 02:35:57 +0200

    create class skeleton and stub method

---
+++ src/main/java/com/example/tddexample/Template.java
@@ -0,0 +1,15 @@
+package com.example.tddexample;
+
+public class Template {
+
+    public Template(String templateText) {
+    }
+
+    public void set(String variable, String value) {
+
+    }
+
+    public String evaluate() {
+        return null;
+    }
+}
