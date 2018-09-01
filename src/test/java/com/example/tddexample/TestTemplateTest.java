09cdf00aa725037c9c02ab8228c73df6b1205e8a
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2018-09-01 02:57:42 +0200

    add more tests

--- src/test/java/com/example/tddexample/TestTemplateTest.java
+++ src/test/java/com/example/tddexample/TestTemplateTest.java
@@ -13,5 +13,19 @@ class TestTemplateTest {
         assertEquals("Hello, Reader", template.evaluate());
     }
 
+    @Test
+    void differentValue() throws Exception {
+        Template template = new Template("Hello, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hello, someone else", template.evaluate());
+    }
 
+    @Test
+    public void multipleVariables() throws Exception {
+        Template template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+        assertEquals("1, 2, 3", template.evaluate());
+    }
 }
