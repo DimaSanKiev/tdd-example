4f1ae9c0ccc0bf5e16602221599d9888662ab452
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2018-09-01 02:57:53 +0200

    implement methods

--- src/main/java/com/example/tddexample/Template.java
+++ src/main/java/com/example/tddexample/Template.java
@@ -1,15 +1,29 @@
 package com.example.tddexample;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Template {
 
+    private Map<String, String> variables;
+    private String templateText;
+
     public Template(String templateText) {
+        this.variables = new HashMap<>();
+        this.templateText = templateText;
     }
 
     public void set(String variable, String value) {
-
+        this.variables.put(variable, value);
     }
 
     public String evaluate() {
-        return "Hello, Reader";
+        String result = templateText;
+
+        for (Map.Entry<String, String> entry : variables.entrySet()) {
+            String regex = "\\$\\{" + entry.getKey() + "\\}";
+            result = result.replaceAll(regex, entry.getValue());
+        }
+        return result;
     }
 }
