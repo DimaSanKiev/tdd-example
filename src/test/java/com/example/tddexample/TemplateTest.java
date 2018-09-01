4c261f75fec3ca9f4548594eeb5d125a51455ec7
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2018-09-01 03:10:25 +0200

    refactor tests

---
+++ src/test/java/com/example/tddexample/TemplateTest.java
@@ -0,0 +1,49 @@
+package com.example.tddexample;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TemplateTest {
+
+    private Template template;
+
+    @Before
+    public void setUp() throws Exception {
+        template = new Template("${one}, ${two}, ${three}");
+        template.set("one", "1");
+        template.set("two", "2");
+        template.set("three", "3");
+    }
+
+    @Test
+    public void oneVariable() throws Exception {
+        Template template = new Template("Hello, ${name}");
+        template.set("name", "Reader");
+        assertEquals("Hello, Reader", template.evaluate());
+    }
+
+    @Test
+    public void differentValue() throws Exception {
+        Template template = new Template("Hello, ${name}");
+        template.set("name", "someone else");
+        assertEquals("Hello, someone else", template.evaluate());
+    }
+
+    @Test
+    public void multipleVariables() throws Exception {
+        assertTeamplateEvaluatesTo("1, 2, 3");
+    }
+
+    @Test
+    public void unknownVariablesAreIgnored() throws Exception {
+        template.set("doesnotexist", "whatever");
+        assertTeamplateEvaluatesTo("1, 2, 3");
+    }
+
+    private void assertTeamplateEvaluatesTo(String expected) {
+        assertEquals(expected, template.evaluate());
+    }
+}
